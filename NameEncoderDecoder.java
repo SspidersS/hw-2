f02753726ab0748fae3542da1ac2004cf8905748
SspidesS <99414766+SspidesS@users.noreply.github.com>
2022-04-26 21:14:58 +0200

    Update NameEncoderDecoder.java

--- NameEncoderDecoder.java
+++ NameEncoderDecoder.java
@@ -2,29 +2,27 @@ class NameEncoderDecoder{
   public String encode(String name){
     return  
              "NOTFORYOU"+  name 
-                .replase("e","1")
-                .replase("u","2")
-                .replase("i","3")
-                .replase("o","4")
-                .replase("a","5")
+                .replace("e","1")
+                .replace("u","2")
+                .replace("i","3")
+                .replace("o","4")
+                .replace("a","5")
            +"YESNOTFORYOU";
     
   }   
   
   
  public String decode(String name){
-   return name  .replase("YESNOTFORYOU","")
-                .replase("NOTFORYOU","")
-                .replase("1","e")
-                .replase("2","u")
-                .replase("3","i")
-                .replase("4","o")
-                .replase("5","a");
+   return name  .replace("YESNOTFORYOU","")
+                .replace("NOTFORYOU","")
+                .replace("1","e")
+                .replace("2","u")
+                .replace("3","i")
+                .replace("4","o")
+                .replace("5","a");
     
 }
   
   
   
-  
-  
-} 
+ } 
